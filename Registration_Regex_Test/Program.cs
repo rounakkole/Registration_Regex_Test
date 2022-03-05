5ef09ae67fa428f947c539786e0407fb9562a0dd
rounak <rounakkole@gmail.com>
2022-03-05 14:55:30 +0530

    Add project files.

---
+++ Registration_Regex_Test/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace Registration_Regex_Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("MS test");
+        }
+    }
+}
